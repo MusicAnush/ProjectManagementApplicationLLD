e312c615ebc747bee8200b0672129b1d42d89ce4
Anush Narayanan <30994627+MusicAnush@users.noreply.github.com>
2022-08-27 19:59:05 +0530

    Update ProjectMgmtApp.py

--- ProjectMgmtApp.py
+++ ProjectMgmtApp.py
@@ -1,37 +1,48 @@
 class Employee:
-  def __init__(self, name : string, age : int, gender : string,  designation : string):
-    self.name = name;
-    self.age = age;
-    self.gender = gender;
-    self.designation = designation;
+    def __init__(self, name: str = "", age: int = 0, gender: str = "", designation: str = ""):
+        self.name = name;
+        self.age = age;
+        self.gender = gender;
+        self.designation = designation;
+
 
 class ProjectTeamLead(Employee):
-  def __init__(self, listOfEmployeesUnderProjectTeamLead : List<Employee> = []):
-    self.listOfEmployeesUnderProjectTeamLead = listOfEmployeesUnderProjectTeamLead;
-    
+    def __init__(self, listOfEmployeesUnderProjectTeamLead: list[Employee] = []):
+        self.listOfEmployeesUnderProjectTeamLead = listOfEmployeesUnderProjectTeamLead;
+
+
 class Manager(Employee):
-  def__init__(self, listOfProjectTeamLeads : List<ProjectTeamLead>):
-    self.listOfProjectTeamLeads = listOfProjectTeamLeads;
+    def __init__(self, listOfProjectTeamLeads: list[ProjectTeamLead] = []):
+        self.listOfProjectTeamLeads = listOfProjectTeamLeads;
+
 
 class Task:
-  def __init__(self, taskName : string = "", expectedHoursToFinishTheTask : int = 0, taskOwner : Employee = None, taskStatus : string = "NotStarted"):
-    self.taskName = taskName;
-    self.expectedHoursToFinishTheTask = expectedHoursToFinishTheTask;
-    self.taskOwner = taskOwner;
-    self.taskStatus = taskStatus;
+    def __init__(self, taskName="", expectedHoursToFinishTheTask=0, taskOwner: Employee = None,
+                 taskStatus="NotStarted"):
+        self.taskName = taskName;
+        self.expectedHoursToFinishTheTask = expectedHoursToFinishTheTask;
+        self.taskOwner = taskOwner;
+        self.taskStatus = taskStatus;
+
 
 class UserStory:
-  def __init__(self, listOfTasks : List<Task> = [], numberOfStoryPoints : int = 0, storyStatus : string = "", storyOwner : Employee = None):
-    self.listOfTasks = listOfTasks;
-    self.numberOfStoryPoints = numberOfStoryPoints;
-    self.storyStatus = storyStatus;
-    self.storyOwner = storyOwner;
-    
+    def __init__(self, listOfTasks: list[Task] = [], numberOfStoryPoints: int = 0,
+                 storyStatus="", storyOwner: Employee = None):
+        self.listOfTasks = listOfTasks;
+        self.numberOfStoryPoints = numberOfStoryPoints;
+        self.storyStatus = storyStatus;
+        self.storyOwner = storyOwner;
+
+
 class Feature:
-  def __init__(self, listOfUserStories, featureOwner, featureSize)
+    def __init__(self, listOfUserStories: list[UserStory] = [], featureOwner: Employee = None, featureSize='XXL'):
+        self.listOfUserStories = listOfUserStories;
+        self.featureOwner = featureOwner;
+        self.featureSize = featureSize;
+
 
 class Project:
-  def __init__(self, projectName : string, listOfFeatures : List<Feature> = [], projectManager : Manager  = None):
-    self.projectName = projectName;
-    self.listOfFeatures = listOfFeatures;
-    self.projectManager = projectManager;
+    def __init__(self, projectName="", listOfFeatures: list[Feature] = [], projectManager: Manager = None):
+        self.projectName = projectName;
+        self.listOfFeatures = listOfFeatures;
+        self.projectManager = projectManager;
