6dfc27b6eb4a77a5b2e36c668f736220ce81945a
Anush Narayanan <mac@macs-MacBook-Pro.local>
2022-08-28 13:26:10 +0530

    Final Improvements

--- ProjectMgmtApp.py
+++ ProjectMgmtApp.py
@@ -7,28 +7,32 @@ class Employee:
     """
     This is a constructor for the Employee class.
     """
-    __mapOfUserStory = dict();
+    __mapOfUserStory = dict()
 
-    def __init__(self, name: str = "", age: int = 0, gender: str = "", designation: str = ""):
-        self.__name = name;
-        self.__age = age;
-        self.__gender = gender;
-        self.__designation = designation;
-        self.__listOfUserStories = list[UserStory];
+    def __init__(self, empId: int = 0, name: str = "", age: int = 0, gender: str = "", designation: str = ""):
+        self.__empId = empId
+        self.__name = name
+        self.__age = age
+        self.__gender = gender
+        self.__designation = designation
+        self.__listOfUserStories = list[UserStory]
+
+    def getEmpId(self):
+        return self.__empId;
 
     def createUserStory(self, userStoryId, numberOfStoryPoints, storyStatus):
-        userStory = UserStory([], numberOfStoryPoints, storyStatus, self);
-        self.__listOfUserStories.append(userStory);
-        self.__mapOfUserStory[userStoryId] = userStory;
+        userStory = UserStory([], numberOfStoryPoints, storyStatus, self)
+        self.__listOfUserStories.append(userStory)
+        self.__mapOfUserStory[userStoryId] = userStory
 
     def createTask(self, taskName="", expectedHoursToFinishTheTask=0,
                    taskStatus="NotStarted", userStoryId: int = 0):
-        userStory = UserStory(self.__mapOfUserStory[userStoryId]);
-        userStory.getListOfTasks().append(Task(taskName, expectedHoursToFinishTheTask, self, taskStatus));
-        self.__mapOfUserStory[userStoryId] = userStory;
+        userStory = UserStory(self.__mapOfUserStory[userStoryId])
+        userStory.getListOfTasks().append(Task(taskName, expectedHoursToFinishTheTask, self, taskStatus))
+        self.__mapOfUserStory[userStoryId] = userStory
 
     def getUserStoryMap(self):
-        return self.__mapOfUserStory;
+        return self.__mapOfUserStory
 
     """
     This is a method that  defines to view the object details in python in the format defined by us in this method.
@@ -37,7 +41,7 @@ class Employee:
     def __str__(self):
         return "name : " + self.name + " age : " + str(
             self.age) + " gender : " + self.gender + " designation : " + self.designation + "listOfUserStories : " \
-               + self.__listOfUserStories;
+               + self.__listOfUserStories
 
 
 """
@@ -53,11 +57,11 @@ class ProjectTeamLead(Employee):
 
     """
 
-    def __init__(self, name: str = "", age: int = 0, gender: str = "", designation: str = "",
+    def __init__(self, empId: int = 0, name: str = "", age: int = 0, gender: str = "", designation: str = "",
                  listOfEmployeesUnderProjectTeamLead: list[Employee] = []):
-        super().__init__(name, age, gender, designation);
-        self.listOfEmployeesUnderProjectTeamLead = listOfEmployeesUnderProjectTeamLead;
-        self.projectAssigned = Project();
+        super().__init__(empId, name, age, gender, designation)
+        self.__listOfEmployeesUnderProjectTeamLead = listOfEmployeesUnderProjectTeamLead
+        self.__projectAssigned = Project()
 
     """
     In this method to print the Project TL (Team Lead)
@@ -65,10 +69,20 @@ class ProjectTeamLead(Employee):
 
     def __str__(self):
         return super.__str__() + "List Of EmployeesUnderProject TeamLead : " + str(
-            list[self.listOfEmployeesUnderProjectTeamLead]);
+            list[self.listOfEmployeesUnderProjectTeamLead])
+
+    def createEmployee(self, empId: int = 0, name: str = "", age: int = 0, gender: str = "", designation: str = ""):
+        self.__listOfEmployeesUnderProjectTeamLead.append(Employee(empId, name, age, gender, designation));
+
+    def assignProject(self, project):
+        self.__projectAssigned = project
+
+    def getProjectTeamLeadId(self):
+        return self.getEmpId()
 
 
 class Manager(Employee):
+    __mapOfProjectTLIdAndProjectAssignedId = dict();
     """
     this is the constructor of the Manager Method. A manager can have list of project team leads.
     It is derived from Employee class, since a Manager is an Employee with some extra privileges.
@@ -76,42 +90,48 @@ class Manager(Employee):
     and can assign exactly one project to one project Team Lead.
     """
 
-    def __init__(self, name: str = "", age: int = 0, gender: str = "", designation: str = "",
+    def __init__(self, empId: int = 0, name: str = "", age: int = 0, gender: str = "", designation: str = "",
                  listOfProjectTeamLeads: list[ProjectTeamLead] = []):
-        super().__init__(name, age, gender, designation);
-        self.__listOfProjects = [];
-        self.__listOfProjectTeamLeads = listOfProjectTeamLeads;
+        super().__init__(empId, name, age, gender, designation)
+        self.__listOfProjects = []
+        self.__listOfProjectTeamLeads = listOfProjectTeamLeads
 
-    def createProjectLead(self):
-        pass;
+    def createProjectLead(self, empId: int = 0, empName: str = "", age: int = 0, gender: str = "",
+                          designation: str = ""):
+        self.__listOfProjectTeamLeads.append(ProjectTeamLead(empId, empName, age, gender, designation))
 
     """
     I have assumed that a manager alone can create a project. and can have multiple projects under he/she. 
     So i have maintained a list of the projects that he / she manages. 
     """
 
-    def createProject(self, projectName):
-        project = Project();
-        # since I have made the project member variables private, it needs to be accessed through
-        # a public method that I have created.
-        project.createProject(projectName, [], self);
-        self.listOfProjects.append(project);
+    def createProject(self, projectId: int = 0, projectName="", projectExpectedDurationForCompletionInYears: int = 0):
+        project = Project(projectId, projectName, [],
+                          self, projectExpectedDurationForCompletionInYears)
+        self.listOfProjects.append(project)
 
     """
     returns the list of projects under the manager.
     """
 
     def getListOfProjects(self):
-        return self.listOfProjects;
+        return self.listOfProjects
+
+    def assignProjectUnderProjectTl(self, projectId, ProjectTLId):
+        self.__mapOfProjectTLIdAndProjectAssignedId[ProjectTLId] = projectId
+        ProjectTeamLead(filter(lambda projectTl:
+                               projectTl.getEmpId() == ProjectTLId, self.__listOfProjectTeamLeads)[0]) \
+            .assignProject(Project(filter(lambda project: project.getProjectId() == projectId,
+                                          self.__listOfProjects)[0]))
 
 
 class Task:
     def __init__(self, taskName="", expectedHoursToFinishTheTask=0, taskOwner: Employee = None,
                  taskStatus="NotStarted"):
-        self.__taskName = taskName;
-        self.__expectedHoursToFinishTheTask = expectedHoursToFinishTheTask;
-        self.__taskOwner = taskOwner;
-        self.__taskStatus = taskStatus;
+        self.__taskName = taskName
+        self.__expectedHoursToFinishTheTask = expectedHoursToFinishTheTask
+        self.__taskOwner = taskOwner
+        self.__taskStatus = taskStatus
 
     def __str__(self):
         return "[taskName : " + self.__taskName + " taskOwner : " + self.__taskOwner + \
@@ -121,27 +141,27 @@ class Task:
 class UserStory:
     def __init__(self, userStoryId: int = 0, listOfTasks: list[Task] = [], numberOfStoryPoints: int = 0,
                  storyStatus="", storyOwner: Employee = None):
-        self.__listOfTasks = listOfTasks;
-        self.__numberOfStoryPoints = numberOfStoryPoints;
-        self.__storyStatus = storyStatus;
-        self.__storyOwner = storyOwner;
-        self.__userStoryId = userStoryId;
+        self.__listOfTasks = listOfTasks
+        self.__numberOfStoryPoints = numberOfStoryPoints
+        self.__storyStatus = storyStatus
+        self.__storyOwner = storyOwner
+        self.__userStoryId = userStoryId
 
     def getListOfTasks(self):
-        return self.__listOfTasks;
+        return self.__listOfTasks
 
 
 class Feature:
     def __init__(self, listOfUserStories: list[UserStory] = [], featureOwner: Employee = None, featureSize='XXL'):
-        self.__listOfUserStories = listOfUserStories;
-        self.__featureOwner = featureOwner;
-        self.__featureSize = featureSize;
+        self.__listOfUserStories = listOfUserStories
+        self.__featureOwner = featureOwner
+        self.__featureSize = featureSize
 
 
 class Sprint:
     def __init__(self, sprintNum: int = 0, listOfFeatures: list[Feature] = []):
-        self.__sprintNum = sprintNum;
-        self.__listOfFeatures = listOfFeatures;
+        self.__sprintNum = sprintNum
+        self.__listOfFeatures = listOfFeatures
 
 
 """
@@ -154,20 +174,29 @@ class Project:
     def __init__(self, projectId: int = 0, projectName="", listOfFeatures: list[Feature] = [],
                  projectManager: Manager = None,
                  projectExpectedDurationForCompletionInYears: int = 0):
-        self.__projectId = projectId;
-        self.__projectName = projectName;
-        self.__listOfFeatures = listOfFeatures;
-        self.__projectManager = projectManager;
-        self.__projectExpectedDurationForCompletionInYears = projectExpectedDurationForCompletionInYears;
-        self.__listOfSprints = [];
+        self.__projectId = projectId
+        self.__projectName = projectName
+        self.__listOfFeatures = listOfFeatures
+        self.__projectManager = projectManager
+        self.__projectExpectedDurationForCompletionInYears = projectExpectedDurationForCompletionInYears
+        self.__listOfSprints = []
 
     def appendSprintDetails(self, sprintDetail: Sprint):
         self.__listOfSprints.append(sprintDetail);
 
+    def getProjectId(self):
+        return self.__projectId
+
 
 class Organization:
     def __init__(self, orgName):
-        self.orgName = orgName;
+        self.orgName = orgName
 
     def createManager(self, managerName: str = ""):
+        pass
+
+    def createEmployee(self, empId, empName, age, gender, designation):
+        pass
+
+    def createProjectLead(self, empName, age, gender, designation):
         pass
